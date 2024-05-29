3f6723428780f5fe672e9a292ae6b5e0df9a15de
Lihy_FM <130652614+Lihy753@users.noreply.github.com>
2024-05-29 18:49:40 +0800

    Update DivideAnd ConquerStrategy.cc

--- 4_Divide and conquer strategy/DivideAnd ConquerStrategy.cc
+++ 4_Divide and conquer strategy/DivideAnd ConquerStrategy.cc
@@ -1,1 +1,42 @@
+#include <iostream>
+#include <limits>
+#include <stdexcept>
+#include <array>
 
+//最大子数组问题
+template<size_t Size>
+class maxSunarrayProblem
+{
+public:
+    maxSunarrayProblem(std::array<int, Size>& array) : m_array(array) {}
+
+    std::array<int, 3> findMaxCrossingSubarray(int low, int mid, int high)
+    {
+        if (!(low <= mid && mid <= high))
+            throw std::invalid_argument("The input sequence of low, mid and high is wrong");
+
+        int left_sum = std::numeric_limits<int>::min();
+        int sum = 0;
+        int max_left = 0;
+        for (int i = mid; i != low - 1; --i)
+        {
+            sum = sum + m_array[i];
+            if (sum > left_sum)
+            {
+                left_sum = sum;
+                max_left = i;
+            }
+        }
+    }
+
+
+private:
+    std::array<int, Size> m_array;
+};
+
+
+int main()
+{
+
+    return 0;
+}

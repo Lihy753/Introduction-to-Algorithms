142e2e36d32b15ad53c8a1cca0ad0d7c0f62ff66
Lihy_FM <130652614+Lihy753@users.noreply.github.com>
2024-05-30 19:28:31 +0800

    Update DivideAnd ConquerStrategy.cc

--- 4_Divide and conquer strategy/DivideAnd ConquerStrategy.cc
+++ 4_Divide and conquer strategy/DivideAnd ConquerStrategy.cc
@@ -16,19 +16,34 @@ public:
             throw std::invalid_argument("The input sequence of low, mid and high is wrong");
 
         int left_sum = std::numeric_limits<int>::min();
-        int sum = 0;
+        int sum_1 = 0;
         int max_left = 0;
+        int max_right = high;
         for (int i = mid; i != low - 1; --i)
         {
-            sum = sum + m_array[i];
-            if (sum > left_sum)
+            sum_1 = sum_1 + m_array[i];
+            if (sum_1 > left_sum)
             {
-                left_sum = sum;
+                left_sum = sum_1;
                 max_left = i;
             }
         }
-    }
 
+        int right_sum = std::numeric_limits<int>::min();
+        int sum_2 = 0;
+        for (int j = mid + 1; j != high + 1; ++j)
+        {
+            sum_2 = sum_2 + m_array[j];
+            if (sum_2 > right_sum)
+            {
+                right_sum = sum_2;
+                max_right = j;
+            }
+        }
+
+        return std::array<int, 3>{max_left, max_right, left_sum + right_sum};
+        
+    }
 
 private:
     std::array<int, Size> m_array;

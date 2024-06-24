7f0493d97bae8945fc53cb2ba3afac13c9ce1f78
Lihy753 <imathfmlhy@gmail.com>
2024-06-24 15:38:49 +0800

    algorithm_4_chapter

--- 4_DivideAndConquerStrategy/DivideAndConquerStrategy.cc
+++ 4_DivideAndConquerStrategy/DivideAndConquerStrategy.cc
@@ -3,13 +3,13 @@
 #include <stdexcept>
 #include <array>
 #include <cmath>
+#include <vector>
 
 //最大子数组问题
-template<size_t Size>
 class maxSunarrayProblem
 {
 public:
-    maxSunarrayProblem(std::array<int, Size>& array) : m_array(array) {}
+    maxSunarrayProblem(std::vector<int> array) : m_array(array) {}
 
     std::array<int, 3> findMaxCrossingSubarray(int low, int mid, int high)
     {
@@ -49,17 +49,22 @@ public:
     std::array<int, 3> findMaximumSubarray(int low, int high)
     {
         if (high == low)
-            return std::array<int, 3>{low, high, A[low]}
+            return std::array<int, 3>{low, high, m_array[low]};
         else
         {
             int mid = static_cast<int>(std::floor(low + high)/2);
             std::array<int, 3> left_sum = findMaximumSubarray(low, mid);
+            std::cout<< "low = " << low << ", right = " << mid << ", sum = " << left_sum[2] << std::endl;
+
             std::array<int, 3> right_sum = findMaximumSubarray(mid + 1, high);
+            std::cout<< "low = " << mid + 1 << ", right = " << high << ", sum = " << right_sum[2] << std::endl;
             std::array<int, 3> cross_sum = findMaxCrossingSubarray(low, mid, high);
 
-            if (left_sum >= right_sum && left_sum >= cross_sum)
+            std::cout<< "low = " << low << ", mid = " << mid << ", right = " << high << ", cross_sum = " << cross_sum[2] << std::endl;
+
+            if (left_sum[2] >= right_sum[2] && left_sum[2] >= cross_sum[2])
                 return left_sum;
-            if (right_sum >= left_sum && right_sum >= cross_sum)
+            if (right_sum[2] >= left_sum[2] && right_sum[2] >= cross_sum[2])
                 return right_sum;
             else    
                 return cross_sum;
@@ -67,13 +72,33 @@ public:
     }
 
 private:
-    std::array<int, Size> m_array;
+    std::vector<int> m_array;
 };
 
 
 
 int main()
 {
+    std::vector<int> input_list;
+    std::cout << "Please input a string of numbers which you want to recorder" << std::endl;
+    int num;
+    while (std::cin >> num)
+    {
+        input_list.push_back(num);
+    }
 
+    std::cout << "Your array is :" << std::endl;
+    std::cout << "[ ";
+    for (int n : input_list) {
+        std::cout << n << " ";
+    }
+    std::cout << "]" << std::endl;
+    int size = input_list.size() - 1;
+    std::array<int, 3> output_result;
+    maxSunarrayProblem sort_1(input_list);
+    output_result = sort_1.findMaximumSubarray(0, size);
+
+    std::cout << "The reordered result is :" << std::endl;
+    std::cout << "max_left = " << output_result[0] << ", max_right = " << output_result[1] << ", max_value = " << output_result[2];
     return 0;
 }

d34942d68d20ce1942dbbcd8263d86d49a2cdd34
Lihy753 <imathfmlhy@gmail.com>
2024-06-25 19:17:16 +0800

    max_heapify

--- 6_HeapSort/Heap.cc
+++ 6_HeapSort/Heap.cc
@@ -6,7 +6,7 @@
 
 
 
-
+template <typename Size>
 class heap
 {
 public:
@@ -25,9 +25,22 @@ public:
         return (2 * i + 1);
     }
 
-    void MAX_HEAPIFY()
+    void MAX_HEAPIFY(std::array<int, Size> A, int i)
     {
+        int l = LEFT(i);
+        int r = RIGHT(i);
+        int length = Size;
+        int largest = 0;
+        if (l <= length && A[l] > A[i])
+            largest = l;
+        else
+            largest =i;
         
+        if (largest != i)
+        {
+            std::swap(A[largest], A[i]);
+            MAX_HEAPIFY(A, largest);
+        }
     }
 };
 

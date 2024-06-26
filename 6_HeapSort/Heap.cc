4c26ec5839f7d390d1cb590748e3538d643804fe
Lihy753 <imathfmlhy@gmail.com>
2024-06-26 18:06:00 +0800

    finish heap sort

--- 6_HeapSort/Heap.cc
+++ 6_HeapSort/Heap.cc
@@ -3,9 +3,12 @@
 #include <algorithm>
 #include <cmath>
 
+using namespace std;
+
 class heap
 {
 public:
+    heap(std::vector<int> input, int size) : arr(input), heap_size(size) {}
     int PARENT(int i)
     {
         return (std::floor((i + 1)/2) - 1);
@@ -21,36 +24,55 @@ public:
         return (2 * i + 2);
     }
 
-    void MAX_HEAPIFY(std::vector<int>& A, int i)
+    void MAX_HEAPIFY(std::vector<int>& arr, int i)
     {
         int l = LEFT(i);
         int r = RIGHT(i);
-        int length = A.size();
+        int length = heap_size;
         int largest = 0;
-        if (l < length && A[l] > A[i])
+        if (l < length && arr[l] > arr[i])
             largest = l;
         else
             largest =i;
 
-        if (r < length && A[r] > A[largest])
+        if (r < length && arr[r] > arr[largest])
             largest = r;
-        
         if (largest != i)
         {
-            std::swap(A[largest], A[i]);
-            MAX_HEAPIFY(A, largest);
+            std::swap(arr[largest], arr[i]);
+            MAX_HEAPIFY(arr, largest);
         }
     }
 
     //将任意输入的一个数组变成一个最大堆数组；
-    void BUILD_MAX_HEAP(std::vector<int>& A)
+    void BUILD_MAX_HEAP()
     {
-        int heap_size = A.size();
         for (int i = std::floor(heap_size/2) - 1; i > 0; --i)
         {
-            MAX_HEAPIFY(A, i);
+            MAX_HEAPIFY(arr, i);
         }
     }
+
+    void HEAPSORT()
+    {
+        BUILD_MAX_HEAP();
+        for (int i = arr.size() - 1; i > 0; --i)
+        {
+            std::swap(arr[0], arr[i]);
+            --heap_size;
+            if (heap_size != 0)
+                MAX_HEAPIFY(arr, 0);
+        }
+    }
+
+    void fetch_result_arr(std::vector<int>& result)
+    {
+        result = arr;
+    }
+
+public:
+    int heap_size;
+    std::vector<int> arr;
 };
 
 int main()
@@ -70,15 +92,16 @@ int main()
     }
     std::cout << "]" << std::endl;
 
-    heap test;
+    heap test(input_list, input_list.size());
 
-    test.BUILD_MAX_HEAP(input_list);
+    test.HEAPSORT();
+    std::vector<int> result;
+    test.fetch_result_arr(result);
     
     std::cout << "reordered array is :" << std::endl;
     std::cout << "[ ";
-    for (int n : input_list) {
+    for (int n : result) {
         std::cout << n << " ";
     }
     std::cout << "]" << std::endl;
-
 }

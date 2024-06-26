cf0c5304856301506cbbd9176d7520f8bba92c5e
Lihy753 <imathfmlhy@gmail.com>
2024-06-26 10:09:22 +0800

    heap

--- 6_HeapSort/Heap.cc
+++ 6_HeapSort/Heap.cc
@@ -1,12 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <cmath>
-#include <random>
-#include <array>
 
-
-
-template <typename Size>
 class heap
 {
 public:
@@ -25,11 +21,11 @@ public:
         return (2 * i + 1);
     }
 
-    void MAX_HEAPIFY(std::array<int, Size> A, int i)
+    void MAX_HEAPIFY(std::vector<int> A, int i)
     {
         int l = LEFT(i);
         int r = RIGHT(i);
-        int length = Size;
+        int length = A.size();
         int largest = 0;
         if (l <= length && A[l] > A[i])
             largest = l;
@@ -42,9 +38,43 @@ public:
             MAX_HEAPIFY(A, largest);
         }
     }
+
+    void BUILD_MAX_HEAP(std::vector<int>& A)
+    {
+        int heap_size = A.size();
+        for (int i = std::floor(heap_size/2) - 1; i > 0; --i)
+        {
+            MAX_HEAPIFY(A, i);
+        }
+    }
 };
 
 int main()
 {
+    std::vector<int> input_list;
+    std::cout << "Please input a string of numbers which you want to recorder" << std::endl;
+    int num;
+    while (std::cin >> num)
+    {
+        input_list.push_back(num);
+    }
+
+    std::cout << "Your array is :" << std::endl;
+    std::cout << "[ ";
+    for (int n : input_list) {
+        std::cout << n << " ";
+    }
+    std::cout << "]" << std::endl;
+
+    heap test;
+
+    test.BUILD_MAX_HEAP(input_list);
+    
+    std::cout << "reordered array is :" << std::endl;
+    std::cout << "[ ";
+    for (int n : input_list) {
+        std::cout << n << " ";
+    }
+    std::cout << "]" << std::endl;
 
 }

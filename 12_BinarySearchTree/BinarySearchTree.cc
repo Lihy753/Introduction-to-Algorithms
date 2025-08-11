21caf4036ae906a88ebbaa46ad2d553de6ababb4
Lihy753 <imathfmlhy@gmail.com>
2025-08-11 17:24:33 +0800

    12 chapter

--- 12_BinarySearchTree/BinarySearchTree.cc
+++ 12_BinarySearchTree/BinarySearchTree.cc
@@ -7,12 +7,46 @@ int main()
     std::vector<int> val_list{15, 6, 3, 2, 4, 7, 13, 9, 18, 17, 20};
 
     BinarySearchTree binary_tree(val_list);
-    binary_tree.Inorder_tree_walk(binary_tree.m_root);
+    TreeNode* root = binary_tree.m_root;
+    binary_tree.Inorder_tree_walk(root);
     std::cout << std::endl;
-    binary_tree.preorder_tree_walk(binary_tree.m_root);
+    binary_tree.preorder_tree_walk(root);
     std::cout << std::endl;
-    binary_tree.postorder_tree_walk(binary_tree.m_root);
+    binary_tree.postorder_tree_walk(root);
+    std::cout << std::endl;
+    TreeNode* result = binary_tree.tree_search(root, 3);
+    std::cout << "result.key " << result->key << endl;
+    std::cout << "result->left.key " << result->left->key << endl;
+    std::cout << "result->right.key " << result->right->key << endl;
+    std::cout << "result->parent.key " << result->parent->key << endl;
+
+    TreeNode* result_2 = binary_tree.iterative_tree_search(root, 3);
+    std::cout << "result_2.key " << result_2->key << endl;
+    std::cout << "result_2->left.key " << result_2->left->key << endl;
+    std::cout << "result_2->right.key " << result_2->right->key << endl;
+    std::cout << "result->parent.key " << result->parent->key << endl;
+
+    TreeNode* min_res = binary_tree.tree_minimum(root);
+    TreeNode* max_res = binary_tree.tree_maximum(root); 
+    std::cout << "min_key " << min_res->key << ", max_res " << max_res->key << std::endl;
+
+    TreeNode* node = binary_tree.tree_search(root, 13);
+    TreeNode* result_3 = binary_tree.tree_successor(node);
+    std::cout << "result_3.key " << result_3->key << std::endl;
+
+    // //这里虽然让 root = 18 的子树挪了过来，但是原本的位置仍然能够遍历到!
+    // TreeNode* node_1 = binary_tree.tree_search(root, 13);
+    // TreeNode* node_2 = binary_tree.tree_search(root, 18);
+    // binary_tree.transplant(node_1, node_2);
+    // binary_tree.Inorder_tree_walk(root);
+    // std::cout << std::endl;
+
+    TreeNode* node_3 = binary_tree.tree_search(root, 7);
+    binary_tree.tree_delete(node_3);
+    binary_tree.Inorder_tree_walk(root);
+    std::cout << std::endl;
 
     
+
     return 0;
 }
--- 12_BinarySearchTree/BinarySearchTree.h
+++ 12_BinarySearchTree/BinarySearchTree.h
@@ -9,10 +9,11 @@ using namespace std;
 
 struct TreeNode
 {
-    TreeNode(int val) : key(val), left(nullptr), right(nullptr){}
+    TreeNode(int val) : key(val), left(nullptr), right(nullptr), parent(nullptr){}
     int key;
     TreeNode* left;
     TreeNode* right;
+    TreeNode* parent;
 };
 
 class BinarySearchTree
@@ -58,6 +59,25 @@ public:
     }
 
     void deleteTree(TreeNode* node);
+
+//12.2  query
+    TreeNode* tree_search(TreeNode* root, int key);
+    
+    TreeNode* iterative_tree_search(TreeNode* root, int key);
+
+    TreeNode* tree_minimum(TreeNode* root);
+
+    TreeNode* tree_maximum(TreeNode* root);
+
+    //后继与前驱， 即节点 x 的后一个节点与前一个节点；
+    TreeNode* tree_successor(TreeNode* x);
+    TreeNode* tree_predecessor(TreeNode* x);
+
+//12.3
+    void tree_insert(TreeNode* z);
+    void transplant(TreeNode* u, TreeNode* v);
+    void tree_delete(TreeNode* z);
+
 public:
     TreeNode* m_root = nullptr;
 };
@@ -97,6 +117,7 @@ void BinarySearchTree::insert(int val)
             if (cur_node->left == nullptr)
             {
                 cur_node->left = new TreeNode(val);
+                cur_node->left->parent = cur_node;
                 success_insert = true;
             }
             else
@@ -109,6 +130,7 @@ void BinarySearchTree::insert(int val)
             if (cur_node->right == nullptr)
             {
                 cur_node->right = new TreeNode(val);
+                cur_node->right->parent = cur_node;
                 success_insert = true;
             }
             else
@@ -133,6 +155,129 @@ void BinarySearchTree::deleteTree(TreeNode* node)
     delete node;
 }
 
+//12.2
+TreeNode* BinarySearchTree::tree_search(TreeNode* root, int key)
+{
+    if (root == nullptr || key == root->key)
+        return root;
+    
+    if (key < root->key)
+        return tree_search(root->left, key);
+    else
+        return tree_search(root->right, key);
+}
 
+TreeNode* BinarySearchTree::iterative_tree_search(TreeNode* root, int key)
+{
+    while (root != nullptr && key != root->key)
+    {
+        if (key < root->key)
+            root = root->left;
+        else
+            root = root->right;
+    }
+    return root;
+}
+
+TreeNode* BinarySearchTree::tree_minimum(TreeNode* root)
+{
+    while (root->left != nullptr)
+    {
+        root = root->left;
+    }
+    return root;
+}
+
+TreeNode* BinarySearchTree::tree_maximum(TreeNode* root)
+{
+    while (root->right != nullptr)
+    {
+        root = root->right;
+    }
+    return root;
+}
+
+TreeNode* BinarySearchTree::tree_successor(TreeNode* x)
+{
+    if (x->right != nullptr)
+        return tree_minimum(x->right);
+    
+    TreeNode* y = x->parent;
+    while (y != nullptr && x == y->right)
+    {
+        x = y;
+        y = y->parent;
+    }
+    return y;
+}
+
+TreeNode* BinarySearchTree::tree_predecessor(TreeNode* x)
+{
+    if (x->left != nullptr)
+        return tree_maximum(x->left);
+    
+    TreeNode* y = x->parent;
+    while (y != nullptr && x == y->left)
+    {
+        x = y;
+        y = y->parent;
+    }
+    return y;
+}
+
+//12.3
+void BinarySearchTree::tree_insert(TreeNode* z)
+{
+    TreeNode* y = nullptr;
+    TreeNode* x = m_root;
+    while (x != nullptr)
+    {
+        y = x;
+        if (z->key < x->key)
+            x = x->left;
+        else
+            x = x->right;
+    }
+    z->parent = y;
+    if (y == nullptr)
+        m_root = y;
+    else if (z->key < y->key)
+        y->left = z;
+    else
+        y->right = z;
+}
+
+void BinarySearchTree::transplant(TreeNode* u, TreeNode* v)
+{
+    if (u->parent == nullptr)
+        m_root = v;
+    else if (u == u->parent->left)
+        u->parent->left = v;
+    else
+        u->parent->right = v;
+    if (v != nullptr)
+        v->parent = u->parent;
+}
+
+void BinarySearchTree::tree_delete(TreeNode* z)
+{   
+    if (z->left == nullptr)
+        transplant(z, z->right);
+    else if (z->right == nullptr)
+        transplant(z, z->left);
+    else 
+    {
+        TreeNode* y = tree_minimum(z->right);
+        if (y->parent != z)
+        {
+            transplant(z, z->right);
+            y->right = z->right;
+            y->right->parent = y;
+        }
+        transplant(z, y);
+        y->left = z->left;
+        y->left->parent = y;
+    }
+}
 
 #endif
